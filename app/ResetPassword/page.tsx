6ff2bedf19f82fea3013215b0374e546ea295506
Marouane <aitkika.marouane@gmail.com>
2024-07-22 15:17:19 +0100

    Suppression de resetPassword

--- app/ResetPassword/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignIn from "./SignIn";
-
-export default function Home() {
-    return ( 
-        <div className="bg-gray-50 min-h-screen">
-            <SignIn /> 
-        </div>
-    );
-}
