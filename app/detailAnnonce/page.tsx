ffd4cb75f4b45f1f6149c9fae64b5a56734de311
Rouknyanas <anas.roukny@gmail.com>
2024-07-24 15:06:38 +0100

    detailprod v2

--- app/detailAnnonce/page.tsx
+++ app/detailAnnonce/page.tsx
@@ -1,134 +1,161 @@
+"use client";
+
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-
+import IconButton from '@mui/material/IconButton';
 
 export default function DetailAnnonce() {
   // Sample product details
   const product = {
     title: "Produit Exemple",
-    price: "50€",
-    description: "Ceci est une description de produit exemple. Ceci est une description de produit exemple. Ceci est une description de produit exemple.",
-    image: "https://via.placeholder.com/300",
+    price: "50",
+    description: "Ceci est une description de produit exemple.Ceci est une description de produit exemple. Ceci est une description de produit exemple. Ceci est une description de produit exempleCeci est une description de produit exemple. Ceci est une description de produit exemple. Ceci est une description de produit exemple Ceci est une description de produit exemple. Ceci est une description de produit exemple.",
+    images: [
+      "https://via.placeholder.com/300?text=Image+1",
+      "https://via.placeholder.com/300?text=Image+2",
+      "https://via.placeholder.com/300?text=Image+3",
+      "https://via.placeholder.com/300?text=Image+4",
+    ],
     type: "Accessoires",
     secteur: "Gueliz",
     etat: "Neuf",
+    payer: "Marrakech",
+    heur: "55 minute",
   };
 
-  return (
-    <Container component="main" maxWidth="md" className="pt-16 mx-auto">
-      <CssBaseline />
-      <Box
-        className="relative flex flex-row items-center bg-white p-6 rounded-lg shadow-lg"
-      >
-        
-        <Button
-          type="button"
-          variant="contained"
-          sx={{
-            position: 'absolute',
-            top: '16px',
-            right: '16px',
-            borderRadius: '50px',
-            '&:hover': { backgroundColor: '#5113D7FF' },
-            '&:active': { backgroundColor: '#3F0EA6FF' },
-            fontSize: '10px', // Smaller font size for the small button
-          }}
-          startIcon={
-            <img
-              src="/icons/x.png" // Path to the image relative to the public directory
-              alt="Icon"
-              style={{ width: '18px', height: '18px' }}
-            />
-          }
-        >
-        </Button>
+  const [selectedImage, setSelectedImage] = React.useState(product.images[0]);
 
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <Avatar
-              alt="Product Image"
-              src={product.image}
-              sx={{ width: 300, height: 300 }}
-              variant="square"
-              className="m-1"
-            />
+  React.useEffect(() => {
+    // Set global styles to ensure full height and width
+    document.documentElement.style.height = '100%';
+    document.documentElement.style.width = '100%';
+    document.body.style.height = '100%';
+    document.body.style.width = '100%';
+    document.body.style.margin = '0';
+    document.body.style.overflow = 'hidden';
+  }, []);
+
+  return (
+    <div style={{ height: '100vh', width: '100vw', margin: 0, padding: 0 }}>
+      <CssBaseline />
+      <Box className="relative flex flex-col bg-white p-6 rounded-lg shadow-lg h-full w-full overflow-hidden">
+        <Box className="absolute top-4 right-4">
+          <img
+            src="/icons/x.png"
+            alt="Close Icon"
+            className="w-7 h-7 cursor-pointer"
+          />
+        </Box>
+
+        <Grid container spacing={2} className="flex-1 h-full mt-10"> {/* Added margin top here */}
+          <Grid item xs={12} md={4} className="pr-4 h-full">
+            <Box className="flex flex-col items-center p-2 h-full">
+              <Avatar
+                alt="Large Product Image"
+                src={selectedImage}
+                className="w-80 h-80 mb-2" // Increase the size here
+                variant="square"
+              />
+              <Box className="flex flex-row overflow-x-auto">
+                {product.images.map((image, index) => (
+                  <IconButton
+                    key={index}
+                    onClick={() => setSelectedImage(image)}
+                    className="p-1 mx-1"
+                  >
+                    <Avatar
+                      alt={`Thumbnail ${index + 1}`}
+                      src={image}
+                      className={`w-16 h-16 ${selectedImage === image ? 'ring-2 ring-purple-600' : ''}`}
+                    />
+                  </IconButton>
+                ))}
+              </Box>
+            </Box>
           </Grid>
-          <Grid item xs={12} md={6} className="flex flex-col justify-between">
-            <div>
+
+          <Grid item xs={12} md={8} className="pl-8 h-full">
+            <div className="pl-4 h-full"> 
               <Typography
-                variant="h4"
-                sx={{ color: '#6D31EDFF' }} // Title color
-                className="font-semibold"
+                variant="h3"
+                className="text-purple-600 font-semibold mb-4" // Added margin bottom here
               >
                 {product.title}
               </Typography>
+              <Box className="flex items-center mt-1">
+                <img
+                  src="/icons/location.png"
+                  alt="Location Icon"
+                  className="w-4 h-4 mr-2"
+                />
+                <Typography
+                  variant="body2"
+                  className="text-black"
+                >
+                  {product.payer}
+                </Typography>
+                <Box className="flex items-center ml-2">
+                  <img
+                    src="/icons/time.png"
+                    alt="Time Icon"
+                    className="w-4 h-4 mr-2"
+                  />
+                  <Typography
+                    variant="body2"
+                    className="text-black"
+                  >
+                    il y a {product.heur}
+                  </Typography>
+                </Box>
+              </Box>
               <Grid container spacing={1}>
                 <Grid item xs={12} md={6}>
-                  <Typography variant="h5" className="text-black">
-                    {product.price}
+                  <Typography className="text-black text-2xl">
+                    {product.price}DH
                   </Typography>
                 </Grid>
               </Grid>
-              <Typography variant="body1" className="mt-2 text-black">
+              <Typography className="mt-2 text-black text-base">
                 {product.description}
               </Typography>
-              <Box mt={2}>
-                <Typography variant="body1" className="text-black">
+              <Box className="mt-2">
+                <Typography className="text-black text-sm">
                   <strong>Type :</strong> {product.type}
                 </Typography>
-                <Typography variant="body1" className="text-black">
+                <Typography className="text-black text-sm">
                   <strong>Secteur :</strong> {product.secteur}
                 </Typography>
-                <Typography variant="body1" className="text-black">
+                <Typography className="text-black text-sm">
                   <strong>Etat :</strong> {product.etat}
                 </Typography>
               </Box>
-            </div>
-            <div className="flex justify-end gap-2 mt-4">
-              <Button
-                type="button"
-                variant="contained"
-                size="small"
-                sx={{
-                  backgroundColor: '#6D31EDFF',
-                  color: '#FFFFFF',
-                  borderRadius: '50px',
-                  '&:hover': { backgroundColor: '#5113D7FF' },
-                  '&:active': { backgroundColor: '#3F0EA6FF' },
-                  width: '200px', 
-                  height: '40px', 
-                  fontSize: '12px',
-                }}
-              >
-                Contactez le vendeur
-              </Button>
-              <Button
-                type="button"
-                variant="contained"
-                size="small"
-                sx={{
-                  backgroundColor: '#6D31EDFF',
-                  color: '#FFFFFF',
-                  borderRadius: '50px',
-                  '&:hover': { backgroundColor: '#5113D7FF' },
-                  '&:active': { backgroundColor: '#3F0EA6FF' },
-                  width: '150px', 
-                  height: '40px',
-                  fontSize: '12px', 
-                }}
-              >
-                Acheter
-              </Button>
+              <div className="flex justify-end gap-4 mt-4"> {/* Use gap-4 for spacing */}
+                <Button
+                  type="button"
+                  variant="contained"
+                  size="small"
+                  className="bg-purple-600 text-white rounded-full hover:bg-purple-700 active:bg-purple-800 w-52 h-10 text-xs"
+                >
+                  Contactez le vendeur
+                </Button>
+                <Button
+                  type="button"
+                  variant="contained"
+                  size="small"
+                  className="bg-purple-600 text-white rounded-full hover:bg-purple-700 active:bg-purple-800 w-40 h-10 text-xs"
+                >
+                  Acheter
+                </Button>
+              </div>
             </div>
           </Grid>
         </Grid>
       </Box>
-    </Container>
+    </div>
   );
 }
