1f7daf1998b3a7f8db9483325f9046ec95b0099c
Marouane <aitkika.marouane@gmail.com>
2024-07-24 12:23:30 +0100

    Fix orthographe signin page

--- app/SignUp/SignUp.tsx
+++ app/SignUp/SignUp.tsx
@@ -58,7 +58,7 @@ export default function SignUp() {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
         <div className="flex justify-center mb-6">
-          <h1 className="text-black text-2xl font-bold">Sinscrire</h1>
+          <h1 className="text-black text-2xl font-bold">S'inscrire</h1>
         </div>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
@@ -122,7 +122,7 @@ export default function SignUp() {
                 name="password"
                 id="password"
                 className="block w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                placeholder="Confirmer le mot de passe"
+                placeholder="Votre mot de passe"
               />
               <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                 <LockIcon style={{ color: 'black' }} />
@@ -149,7 +149,7 @@ export default function SignUp() {
                 name="confirmPassword"
                 id="confirmPassword"
                 className="block w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                placeholder="Choisissez un mot de passe"
+                placeholder="Confirmer le mot de passe"
               />
               <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                 <LockIcon style={{ color: 'black' }} />
@@ -171,7 +171,7 @@ export default function SignUp() {
               type="submit"
               className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              Sign in
+              Créer un compte
             </button>
           </div>
           <div className="mt-6 relative">
