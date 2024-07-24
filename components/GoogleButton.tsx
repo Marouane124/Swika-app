93a36bcf7dc433fd6d600d30a36c12c81683b338
Marouane <aitkika.marouane@gmail.com>
2024-07-24 10:20:27 +0100

    Changement du design de sign in

--- components/GoogleButton.tsx
+++ components/GoogleButton.tsx
@@ -10,7 +10,7 @@ export default function GoogleSignInButton() {
   return (
     <button
       onClick={handleClick}
-      className="w-full flex items-center justify-center h-14 px-6 mt-4 text-sm transition-colors duration-300 bg-white border-2 border-gray text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
+      className="w-full flex items-center justify-center h-12 px-6 mt-4 text-sm transition-colors duration-300 bg-white border-2 border-gray text-black rounded-lg focus:shadow-outline hover:bg-slate-200"
     >
       <Image src={googleLogo} alt="Google Logo" width={20} height={20} />
       <span className="ml-4">Se connecter avec Google</span>
